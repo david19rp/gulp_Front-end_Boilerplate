c615f0d29da24577af286ca81c23af0f873751c1
david <davidramirezperez@outlook.com>
2019-09-05 16:24:49 -0600

    Update gulpfile.js
    updating the gulpfile structure, simple is better!

--- gulpfile.js
+++ gulpfile.js
@@ -1,110 +1,36 @@
-// variable section
-const {
-    dest,
-    series,
-    src,
-    watch
-} = require('gulp')
-const autoprefixer = require('gulp-autoprefixer')
-const browsersync = require("browser-sync").create()
-const concat = require("gulp-concat")
-const del = require("del")
-const nunjucksRender = require("gulp-nunjucks-render")
-const sass = require('gulp-sass')
-const sourcemaps = require('gulp-sourcemaps')
-
-const dist = "dist/"
-const source = "src/"
-
-const css = { in: source + "sass/app.scss",
-    out: dist + "css/",
-    sassOpts: {
-        outputStyle: "compressed",
-        errLogToConsole: true
-    },
-    autoprefixerOpts: {
-        browsers: ['last 2 versions', '> 2%']
-    },
-    watch: source + "sass/**/*"
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
+//compile scss into css
+function style() {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream());
 }
 
-const js = { in: source + "scripts/**/*.js",
-    out: dist + "js/",
-    watch: source + "scripts/**/*"
+function jsFiles() {
+    return gulp.src('./app/js/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream());
 }
 
-
-const nunjuck = { in: source + "pages/**/*.html",
-    out: dist,
-    path: source + "templates",
-    watch: [source + "pages/**/*.html", source + "templates"]
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            // index: "./index.html"
+        }
+    });
+    gulp.watch('app/scss/**/*.scss', style)
+    gulp.watch('app/js/**/*.js', jsFiles)
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('/.all.js').on('change', browserSync.reload);
 }
-
-const syncOpts = {
-    server: {
-        baseDir: dist,
-        index: "index.html"
-    },
-    open: true,
-    notify: true
-}
-
-
-// task section
-
-/*------- Clean Task -------*/
-function clean(cb) {
-    del([dist + "*"])
-    cb()
-}
-/*------- Clean Task -------*/
-
-/*------- Style Task -------*/
-function style(cb) {
-    src(css.in)
-        .pipe(sourcemaps.init())
-        .pipe(sass(css.sassOpts))
-        .pipe(autoprefixer(css.autoprefixerOpts))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest(css.out))
-    watch(css.watch, series(style, browsersync.reload))
-    cb()
-}
-/*------- Style Task -------*/
-
-
-/*------- Script Task -------*/
-function script(cb) {
-    src(js.in)
-        .pipe(sourcemaps.init())
-        .pipe(concat("app.js"))
-        .pipe(sourcemaps.write("."))
-        .pipe(dest(js.out))
-    watch(js.watch, series(script, browsersync.reload))
-    cb()
-}
-/*------- Script Task -------*/
-
-/*------- HTML Task -------*/
-function html(cb) {
-    src(nunjuck.in)
-        .pipe(
-            nunjucksRender({
-                path: nunjuck.path
-            })
-        )
-        .pipe(dest(nunjuck.out))
-    watch(nunjuck.watch, series(html, browsersync.reload))
-    cb()
-}
-/*------- HTML Task -------*/
-
-/*------- Browser Sync Task -------*/
-function bSync(cb) {
-    browsersync.init(syncOpts)
-    cb()
-}
-/*------- Browser Sync Task -------*/
-
-/*------- Default Task -------*/
-exports.default = series(clean, style, script, html, bSync)
+exports.style = style;
+exports.jsFiles = jsFiles;
+exports.watch = watch;
